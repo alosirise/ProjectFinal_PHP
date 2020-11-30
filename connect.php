ac2d5ac12c2041578affd81d3fadaa2a750c8314
alosirise <www.sompod@hotmail.com>
2020-11-30 07:09:48 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 <?php ob_start();?>
 <?php 
-  $conn = new mysqli('localhost','root','','basic');
+  $conn = ftp_connect('ftp.chochiang.com','clt2_as@chochiang.com','mmy91ezUZ','basic');
+//   $conn = new mysqli('localhost','root','','basic');
   mysqli_query($conn,"set character set utf8");
-  if($conn->connect_errno){
-      die("Connection Filed". $conn->connect_error);
-  }
+//   if($conn->connect_errno){
+//       die("Connection Filed". $conn->connect_error);
+//   }
    
 ?>
